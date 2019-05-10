5d4975b07da78b777d7078a0c03424fdf1f94ae3
mohakamg <mohak.kant1amg@gmail.com>
2019-05-10 10:40:43 -0500

    Improved Styling

--- boundary.js
+++ boundary.js
@@ -1,13 +1,14 @@
 class Boundary {
 
-  constructor(x1, y1, x2, y2){
+  constructor(x1, y1, x2, y2, wallColor = color(194, 224, 74)){
     this.a = createVector(x1, y1);
     this.b = createVector(x2, y2);
+    this.wallColor = wallColor;
   }
 
   show(){
     push();
-    stroke(color(194, 224, 74));
+    stroke(this.wallColor);
     strokeWeight(2);
     line(this.a.x, this.a.y, this.b.x, this.b.y);
     pop();
